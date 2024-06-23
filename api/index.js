390073fa1e7ffcab94028c60e6c4c9c20dd0c6d8
Kuldeep Rathod <kuldiprathod375@gmail.com>
2024-06-23 22:40:50 +0530

    blog app

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import authRouter from "./routes/auth.js";
+import userRouter from "./routes/users.js";
+import postRouter from "./routes/posts.js";
 
 const app = express()
 
 app.use(express.json())
+app.use("/api/auth", authRouter)
+app.use("/api/users", userRouter)
+app.use("/api/posts", postRouter)
+
 
 app.listen(8800, () => {
     console.log("server is working")
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
+
 
 function Register() {
+    const [inputs, setInputs] = useState({
+        username:"",
+        email:"",
+        password:"",
+    })
+
+    const handleChange = e => {
+        setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        try{
+            const res = await axios.post("http://localhost:5173/auth/register", inputs)
+            console.log(res)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
     return (
         <div className="auth">
             <h1>Register</h1>
             <form action="">
-                <input required type="text" placeholder='Username'/>
-                <input required type="email" placeholder='Email'/>
-                <input required type="password" placeholder='Password'/>
-                <button>Register</button>
+                <input required type="text" placeholder='Username' name='username' onChange={handleChange} />
+                <input required type="email" placeholder='Email' name='email' onChange={handleChange} />
+                <input required type="password" placeholder='Password' name='password' onChange={handleChange} />
+                <button onClick={handleSubmit}>Register</button>
                 <span>
                     Do you have an account? <Link to="/login">Login</Link>
                 </span>
