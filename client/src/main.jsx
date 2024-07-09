22562a79efe51ca3f3f47b498e2b0be9ad546439
Kuldeep Rathod <kuldiprathod375@gmail.com>
2024-07-10 01:01:01 +0530

    backent on vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { AuthContext, AuthContextProvider} from './context/authContext.jsx'
 
 // export const server = "http://localhost:8800/api"
 
-export const server = "https://blog-app-api-one.vercel.app/api"
+export const server = "https://blog-app-api-jet.vercel.app/api"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
