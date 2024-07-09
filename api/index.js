193bfb3ac1cd0cdb2e475d5cbe25c7b074ac81a0
Kuldeep Rathod <kuldiprathod375@gmail.com>
2024-07-09 17:35:00 +0530

    change database to cloud

--- api/index.js
+++ api/index.js
@@ -30,6 +30,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+app.get("/", (req, res)=>{
+  res.send("Welcome to my Blog App")
+})
+
 app.post(`/api/upload`, upload.single("file"), function (req, res) {
   const file = req.file
   res.status(200).json(file.filename);
